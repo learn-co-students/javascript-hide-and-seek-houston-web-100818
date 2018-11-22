26cb5cbd886cfbeb92d837f1100c49a1d332588f
barbaradecares <barbaradecares@hotmail.com>
2018-11-22 01:11:11 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
 
+function nestedTarget() {
+    return document.querySelector( '#nested .target')
+}
+
+function deepestChild() {
+    return document.querySelector('#grand-node div div div div')
+    
+}
+
+function increaseRankBy(n) {
+    const ranked = document.querySelectorAll( '.ranked-list li') 
+    for (let i = 0; i < ranked.length; i++) {
+        ranked[i].innerHTML = parseInt(ranked[i].innerHTML, 10) + n;
+    }
+}
