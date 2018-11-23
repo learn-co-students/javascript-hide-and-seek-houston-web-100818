f69a4ff90b79abccb87396a74a6a8037beb49005
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-11-23 17:18:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(selector) {
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div");
+}
+
+function increaseRankBy(n) {
+  let ranked = document.querySelectorAll(".ranked-list");
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n;
+  }
+  return ranked;
+}
